d2f1220da662e05e90274aab372acc99b664da17
Amha Geleta <amha.nurilligne@gmail.com>
2022-07-05 06:56:12 -0500

    city images

--- client2/src/components/featured/Featured.jsx
+++ client2/src/components/featured/Featured.jsx
@@ -14,7 +14,7 @@ const Featured = () => {
         <>
           <div className="featuredItem">
             <img
-              src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
+              src="https://upload.wikimedia.org/wikipedia/commons/a/a3/Addis_ababa_night_skyline.jpg"
               alt=""
               className="featuredImg"
             />
@@ -26,7 +26,7 @@ const Featured = () => {
 
           <div className="featuredItem">
             <img
-              src="https://cf.bstatic.com/xdata/images/city/max500/690334.webp?k=b99df435f06a15a1568ddd5f55d239507c0156985577681ab91274f917af6dbb&o="
+              src="https://www.worqambatour.com/img/slider_single_tour/Bahrdar%20_1000x667.jpg"
               alt=""
               className="featuredImg"
             />
@@ -37,7 +37,7 @@ const Featured = () => {
           </div>
           <div className="featuredItem">
             <img
-              src="https://cf.bstatic.com/xdata/images/city/max500/689422.webp?k=2595c93e7e067b9ba95f90713f80ba6e5fa88a66e6e55600bd27a5128808fdf2&o="
+              src="https://i.pinimg.com/564x/a7/09/c9/a709c9b359f95727c3484fddf0a9880c.jpg"
               alt=""
               className="featuredImg"
             />
